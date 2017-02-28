e89e5fcff3499f4e801f05881fffac21839b91db
Tomasz Gieniusz <tomasz.gieniusz@ihs.com>
2017-02-28 12:14:35 +0100

    fitlering by categories

--- SportsStore.WebUI/Controllers/ProductController.cs
+++ SportsStore.WebUI/Controllers/ProductController.cs
@@ -15,11 +15,12 @@ namespace SportsStore.WebUI.Controllers
             _repository = productRepository;
         }
 
-        public ViewResult List(int page = 1)
+        public ViewResult List(string category, int page = 1)
         {
             var model = new ProductsListViewModel
             {
                 Products = _repository.Products
+                                      .Where(p => category == null || p.Category == category)
                                       .OrderBy(p => p.ProductID)
                                       .Skip((page - 1) * PageSize)
                                       .Take(PageSize),
@@ -27,8 +28,11 @@ namespace SportsStore.WebUI.Controllers
                 {
                     CurrentPage = page,
                     ItemsPerPage = PageSize,
-                    TotalItems = _repository.Products.Count()
-                }
+                    TotalItems = category == null ?
+                        _repository.Products.Count() :
+                        _repository.Products.Count(e => e.Category == category)
+                },
+                CurrentCategory = category
             };
             return View(model);
         }
--- SportsStore.WebUI/Models/ProductsListViewModel.cs
+++ SportsStore.WebUI/Models/ProductsListViewModel.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
 using SportsStore.Domain.Entities;
 
 namespace SportsStore.WebUI.Models
@@ -10,5 +7,6 @@ namespace SportsStore.WebUI.Models
     {
         public IEnumerable<Product> Products { get; set; }
         public PagingInfo PagingInfo { get; set; }
+        public string CurrentCategory { get; set; }
     }
 }
