cc2b8091c632c010ffdbe4cbfbc8ab2defd1e125
Tomasz Gieniusz <tomasz.gieniusz@ihs.com>
2017-02-28 12:29:50 +0100

    simple cart

---
+++ SportsStore.Domain/Entities/Cart.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SportsStore.Domain.Entities
+{
+    public class Cart
+    {
+        private readonly List<CartLine> _lineCollection = new List<CartLine>();
+
+        public void AddItem(Product product, int quantity)
+        {
+            var line = _lineCollection.FirstOrDefault(p => p.Product.ProductID == product.ProductID);
+
+            if (line == null)
+            {
+                _lineCollection.Add(new CartLine
+                {
+                    Product = product,
+                    Quantity = quantity
+                });
+            }
+            else
+            {
+                line.Quantity += quantity;
+            }
+        }
+
+        public void RemoveLine(Product product)
+        {
+            _lineCollection.RemoveAll(l => l.Product.ProductID == product.ProductID);
+        }
+
+        public decimal ComputeTotalValue()
+        {
+            return _lineCollection.Sum(e => e.Product.Price * e.Quantity);
+        }
+
+        public void Clear()
+        {
+            _lineCollection.Clear();
+        }
+
+        public IEnumerable<CartLine> Lines => _lineCollection;
+    }
+
+    public class CartLine
+    {
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+    }
+}
